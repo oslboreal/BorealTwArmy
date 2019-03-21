f190777ade32648942e3a9ddb8f4c33a61bdb4b4
Vallejo Juan Marcos <oslboreal@gmail.com>
2019-03-21 10:40:36 -0300

    Design diagram.

---
+++ BorealTwArmy/ProcesoPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace BorealTwArmy
+{
+    /// <summary>
+    /// Singleton design pattern.
+    /// </summary>
+    class MainProcess
+    {
+        internal MainProcess Process { get; set; }
+
+        private MainProcess() { }
+
+        /// <summary>
+        /// Start a new process.
+        /// </summary>
+        private void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Stop the process.
+        /// </summary>
+        private void Stop()
+        {
+
+        }
+    }
+}
